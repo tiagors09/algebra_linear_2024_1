d6737acea478f80786eb8b3a9cc21e1337c13f9a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-02-27 10:59:58 -0300

    adicionado arquivo da aula 1 - 27/02/2024

---
+++ aula_1.m
@@ -0,0 +1,28 @@
+x = [
+1 2 3;
+2 5 3
+]
+
+size(x)
+
+a = [
+2 3 4;
+3 4 5;
+4 5 6
+]
+
+b = randi(9,3,3)
+
+c = eye(3)
+
+x = randi(9,3,3)
+y = randi(9,3,3)
+
+resul_soma_x_y = x + y
+
+resul_sub_x_y = x - y
+
+z = [1 2]
+w = [2; 3]
+
+resul_soma_z_w = z + w
